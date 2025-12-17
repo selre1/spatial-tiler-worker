dec3342a876c1341a1c820ee8cc12d84e6910bb6
bhb <selre1@naver.com>
2025-12-17 18:02:32 +0900

    # 의존성명 변경

--- scripts/install_offline.bat
+++ scripts/install_offline.bat
@@ -12,12 +12,12 @@ if not exist venv (
 call venv\Scripts\activate
 
 REM wheelhouse에서만 설치하도록 강제
-pip install -r requirements-offline.txt
+pip install -r requirements.txt
 
 REM 3) py3dtilers 자체 설치
 pip install -e .
 
 echo.
 echo install done.
-echo Try: ifc-tiler -i ".\test.ifc" -o ".\output" --crs_in EPSG:5186 --crs_out EPSG:4978
+echo Try: (venv) ifc-tiler -i ".\test.ifc" -o ".\output" --crs_in EPSG:5186 --crs_out EPSG:4978
 endlocal
